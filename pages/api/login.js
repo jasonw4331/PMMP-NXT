19376f781b20d9041c6657dc5dca977997e23891
jasonwynn10 <jasonwynn10@me.com>
2021-12-06 00:31:31 -0500

    Created a RESTful endpoint with user auth support

---
+++ pages/api/login.js
@@ -0,0 +1,17 @@
+import { setAuthCookies } from 'next-firebase-auth'
+import initAuth from "../../lib/firebase/initAuth";
+
+initAuth()
+
+const handler = async (req, res) => {
+  try {
+    await setAuthCookies(req, res)
+  } catch (e) {
+    // eslint-disable-next-line no-console
+    console.error(e)
+    return res.status(500).json({ error: 'Unexpected error.' })
+  }
+  return res.status(200).json({ status: true })
+}
+
+export default handler
---
+++ pages/api/logout.js
@@ -0,0 +1,17 @@
+import { unsetAuthCookies } from 'next-firebase-auth'
+import initAuth from "../../lib/firebase/initAuth";
+
+initAuth()
+
+const handler = async (req, res) => {
+  try {
+    await unsetAuthCookies(req, res)
+  } catch (e) {
+    // eslint-disable-next-line no-console
+    console.error(e)
+    return res.status(500).json({ error: 'Unexpected error.' })
+  }
+  return res.status(200).json({ status: true })
+}
+
+export default handler
