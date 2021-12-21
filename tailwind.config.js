eabdf26ecf8a5c9305bc7f0aa556bd83137aaa59
jasonwynn10 <jasonwynn10@me.com>
2021-12-21 12:38:36 -0500

    Use tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  mode: 'jit',
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/forms'),
+  ],
+}
