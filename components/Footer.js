c960e05df70ba3c669f7878d8f8a5c89f01c24b0
jasonwynn10 <jasonwynn10@me.com>
2021-12-22 17:08:20 -0500

    specify footer text formatting

--- components/Footer.js
+++ components/Footer.js
@@ -2,7 +2,7 @@ import Link from 'next/link'
 
 const Footer = () => {
   return (
-    <footer>
+    <footer className="font-normal text-sm text-zinc-500">
       <center>
         <h2>&copy; 2021 PMMP-NXT</h2>
         <h5>Some Icons by <Link href="https://freepik.com"><a>freepik.com</a></Link> and <Link href="https://icons8.com"><a>icons8.com</a></Link></h5>
