77c42295c3bd90f7263c688c9bfea121716ac00a
jasonwynn10 <jasonwynn10@me.com>
2022-01-06 23:46:32 -0500

    Update next config to allow external image sources

--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@ const withPWA = require('next-pwa')
 
 module.exports = withPWA({
   reactStrictMode: true,
+  images: {
+    domains: ['lh3.googleusercontent.com', 'raw.githubusercontent.com'],
+  },
   dynamicStartUrl: false,
   buildExcludes: [
     /chunks\/images\/.*$/,
