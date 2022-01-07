15a7317144af0fe0fcb59e6ea2bf45c880f80f6f
jasonwynn10 <jasonwynn10@me.com>
2022-01-06 23:34:27 -0500

    Added basic authentication page

---
+++ pages/auth.js
@@ -0,0 +1,18 @@
+import {AuthAction, withAuthUser, withAuthUserTokenSSR,} from 'next-firebase-auth'
+import FirebaseAuth from "../components/FirebaseAuth";
+import Metatags from "../components/Metatags";
+
+const Auth = () => (
+  <div className="top-0">
+    <Metatags title='Log In'/>
+    <FirebaseAuth/>
+  </div>
+)
+
+export const getServerSideProps = withAuthUserTokenSSR({
+  whenAuthed: AuthAction.REDIRECT_TO_APP,
+})()
+
+export default withAuthUser({
+  whenAuthed: AuthAction.REDIRECT_TO_APP,
+})(Auth)
