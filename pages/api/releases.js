24552482e0d7f439f93f449c380d02270bcc6733
jasonwynn10 <jasonwynn10@me.com>
2022-01-07 00:54:32 -0500

    Finished releases API backend

--- pages/api/releases.js
+++ pages/api/releases.js
@@ -1,45 +1,45 @@
 import {getFirebaseAdmin} from 'next-firebase-auth'
 import initAuth from '../../lib/firebase/initAuth'
-import {postToJSON} from "../../lib/firebase/firestoreFuncs";
-import semver from "semver";
+import semver from 'semver'
+import {postToJSON} from '../../lib/firebase/firestoreFuncs'
 
 initAuth()
 
 const handler = async (req, res) => {
-  if(req.method !== 'GET' && req.method !== 'POST') {
-    return res.status(400).json({ error: 'Bad request method' })
+  if (req.method !== 'GET' && req.method !== 'POST') {
+    return res.status(400).json({error: 'Bad request method'})
   }
 
   try {
-    const { latestOnly = undefined, name = undefined, author = undefined } = req.body
+    const {latestOnly = false, name, author} = req.body
 
     const snapshot = await getFirebaseAdmin().firestore().collectionGroup('plugins').get()
     let latestVersions = []
     let docs = []
     snapshot.docs.forEach((doc) => {
-      if(name !== undefined && !doc.id.includes(name)) {
+      if (name && !doc.id.includes(name)) {
         return
       }
-      const { author: author1, releaseStatus } = postToJSON(doc)
+      const data = postToJSON(doc)
       const name1 = doc.id.split('_v')[0]
       const version = doc.id.split('_v')[1]
-      if(releaseStatus <= 1) { // ignore drafts
+      if (data.releaseStatus <= 1) { // ignore drafts
         return
       }
-      if(author !== undefined && author !== author1) {
+      if (author && data.author !== author) {
         return
       }
-      if(latestOnly === true) {
-        if(latestVersions[name1] === undefined || semver.satisfies(version, '>' + latestVersions[name], { includePrerelease: true })) {
+      if (latestOnly === true) {
+        if (latestVersions[name1] === undefined || semver.satisfies(version, '>' + latestVersions[name], {includePrerelease: true})) {
           latestVersions[name1] = version
-          docs[name1] = doc
+          latestVersions.push(version)
+          docs.push(data)
+          return
         }
-      }else{
-        docs[name1] = doc
-        docs.push(doc)
       }
+      docs.push(data)
     })
-    return res.status(200).json(docs.map(doc => postToJSON(doc)))
+    return res.status(200).json({docs})
   } catch (e) {
     return res.status(400).json({ error: e.message })
   }
