d00c2c0581a4d1a8642f22196b41739b5f138ccf
jasonwynn10 <jasonwynn10@me.com>
2022-01-07 04:29:20 -0500

    Started work on plugin details page

--- pages/plugin/[username]/[plugin].js
+++ pages/plugin/[username]/[plugin].js
@@ -1,42 +1,66 @@
-export const getStaticPaths = async () => {
-  const res = await fetch('') // TODO: server-side firestore data lookup (all records)
-  const data = res.json()
+import {AuthAction, useAuthUser, withAuthUser, withAuthUserTokenSSR} from 'next-firebase-auth'
+import Metatags from '../../../components/Metatags'
+import getAbsoluteURL from '../../../lib/getAbsoluteURL'
+import toast from 'react-hot-toast'
+import {serialize} from 'next-mdx-remote/serialize'
+import {MDXRemote} from 'next-mdx-remote'
 
-  // map data to an array of path objects with params (id)
-  const paths = data.map(plugin => {
-    return {
-      params: { username: plugin.author, plugin: plugin.name }
-    }
-  })
-
-  return {
-    paths,
-    fallback: false
-  }
-}
-
-export const getStaticProps = async (context) => {
-  const { username, plugin } = context.params
-  const res = await fetch('') // TODO: server-side firestore data lookup (single plugin)
-  const pluginDetails = await res.json()
-
-  return {
-    props: { pluginDetails }
-  }
-}
-
-const Releases = ({ pluginDetails }) => {
+const PluginData = ({data = [], markdown}) => {
+  const AuthUser = useAuthUser()
   return (
-    <div>
-      <h1>{ pluginDetails.name }</h1>
-      <p>{ pluginDetails.author }</p>
-      <p>{ pluginDetails.version }</p>
-      <p>{ pluginDetails.tagline }</p>
-      <p>{ pluginDetails.description }</p>
-      <p>{ pluginDetails.api }</p>
-      <p>{ pluginDetails.downloadUrl }</p>
-    </div>
-  );
+    <main>
+      <Metatags title={data[0].name}/>
+      <article className="prose prose-zinc lg:prose-xl">
+        <MDXRemote {...markdown} />
+      </article>
+    </main>
+  )
 }
 
-export default Releases;
+export const getServerSideProps = withAuthUserTokenSSR({
+  whenUnauthed: AuthAction.REDIRECT_TO_LOGIN,
+})(async ({AuthUser, req, query}) => {
+  // Optionally, get other props.
+  // You can return anything you'd normally return from
+  // `getServerSideProps`, including redirects.
+  // https://nextjs.org/docs/basic-features/data-fetching#getserversideprops-server-side-rendering
+  const token = await AuthUser.getIdToken()
+  const endpoint = getAbsoluteURL('/api/pluginData', req)
+  const response = await fetch(endpoint, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: token || 'unauthenticated',
+    },
+    body: JSON.stringify({
+      latestOnly: true,
+      name: query.plugin,
+      author: query.username,
+    })
+  })
+  const data = await response.json()
+  if (data)
+    console.log(data)
+  if (!response.ok) {
+    toast.error(JSON.stringify(data))
+    return {
+      props: {
+        data: [
+          {
+            description: "Not OK"
+          }
+        ]
+      }
+    }
+  }
+  return {
+    props: {
+      data: data.docs,
+      markdown: await serialize(data.docs[0].description)
+    },
+  }
+})
+
+export default withAuthUser({
+  whenUnauthedAfterInit: AuthAction.REDIRECT_TO_LOGIN,
+})(PluginData)
