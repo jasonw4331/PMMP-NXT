38b2cc4688393ca65a6800b0ca244840abeb255f
jasonwynn10 <jasonwynn10@me.com>
2022-01-15 06:07:33 -0500

    Updated 404 page to have Metatags

--- pages/404.js
+++ pages/404.js
@@ -1,6 +1,7 @@
 import Link from 'next/link'
 import {useEffect} from 'react'
 import {useRouter} from 'next/router'
+import Metatags from "../components/Metatags";
 
 const NotFound = () => {
   const router = useRouter()
@@ -10,11 +11,14 @@ const NotFound = () => {
   })
 
   return (
-    <div className="not-found">
-      <h1>Ooops...</h1>
-      <h2>That page cannot be found :(</h2>
-      <p>Click to go back to the <Link href="/"><a>Homepage</a></Link></p>
-    </div>
+    <>
+      <Metatags title='Not Found' tagline={'Whoops! That page couldn\'t be found.'}/>
+      <div className="not-found">
+        <h1>Ooops...</h1>
+        <h2>That page cannot be found :(</h2>
+        <p>Click to go back to the <Link href="/"><a>Homepage</a></Link></p>
+      </div>
+    </>
   );
 }
 
