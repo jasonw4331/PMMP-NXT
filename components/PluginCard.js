b1fdd85028f9a6ae5ee1301814961ea7f4491667
jasonwynn10 <jasonwynn10@me.com>
2022-01-23 03:35:54 -0500

    Redesigned plugin card to be closer to figma mockup

--- components/PluginCard.js
+++ components/PluginCard.js
@@ -10,26 +10,36 @@ const PluginCard = ({
   iconUrl = missingImage,
 }) => {
   return (
-    <li className='h-[382px] w-[281px] p-2 flex flex-col justify-between items-center bg-zinc-800 bg-cover text-white rounded-lg mb-10 mr-5 last:mr-0'>
-      <h1 className='w-full px-4 mb-2 font-roboto truncate text-lg'>
+    <li
+      className={
+        'w-[281px] h-[382px] flex flex-col justify-start items-center bg-zinc-800 bg-cover text-white rounded mb-10 mr-5 tracking-wide'
+      }>
+      <h1 className={'w-full h-6 px-4 mt-3 font-roboto text-xl leading-6'}>
         {name.split('_v')[0]}
       </h1>
-      <h2 className='w-full px-5 mb-2 text-zinc-400 font-roboto truncate text-sm'>
-        {author}
-      </h2>
-      <div className='w-[208px] h-[208px] mb-2'>
-        <Image src={iconUrl} alt='Plugin Icon' width={208} height={208} />
+      <h2 className={'w-full h-5 px-4 mb-2 font-roboto text-sm'}>{author}</h2>
+      <div className={'w-[205px] h-[205px] mb-5'}>
+        <Image src={iconUrl} alt='Plugin Icon' width={205} height={205} />
       </div>
-      <p className='w-5/6 h-[48px] break-words text-clip overflow-hidden'>
+      <p
+        className={
+          'h-9 px-4 mb-3 break-words text-sm text-clip overflow-hidden'
+        }>
         {tagline}
       </p>
-      <div className='h-9 flex justify-center gap-2'>
-        <button className='text-purple-400 p-2 font-roboto uppercase'>
+      <div className={'w-72 h-9 flex justify-center gap-3'}>
+        <button
+          className={
+            'text-purple-400 p-2 font-roboto uppercase text-sm leading-4 tracking-widest'
+          }>
           <Link href={'/plugin/' + encodeURI(author) + '/' + encodeURI(name)}>
             <a>More Details</a>
           </Link>
         </button>
-        <button className='text-purple-400 p-2 font-roboto uppercase '>
+        <button
+          className={
+            'text-purple-400 p-2 font-roboto uppercase text-sm leading-4 tracking-widest'
+          }>
           <Link href={downloadUrl}>
             <a>Download</a>
           </Link>
