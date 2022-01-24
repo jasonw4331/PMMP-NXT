a0d57638c3d553de9e02424eede78ac97612a117
jasonwynn10 <jasonwynn10@me.com>
2022-01-24 07:25:51 -0500

    disabled email/passw auth for now

--- components/FirebaseAuth.js
+++ components/FirebaseAuth.js
@@ -126,6 +126,7 @@ const SignInButtons = () => {
     <div className={'max-w-sm'}>
       <button
         onClick={signInWithEmail}
+        disabled={true}
         className='w-full text-white bg-gray-800 hover:bg-gray-900 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 inline-flex items-center justify-center dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-800 dark:border-gray-700'>
         <Password className={'mr-2 -ml-1 w-4 h-4'} />
         Sign in with Email and Password
