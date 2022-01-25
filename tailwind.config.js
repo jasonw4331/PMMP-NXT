dd8599b51fdf3846a6ce8e27ee5ea482656ad124
jasonwynn10 <jasonwynn10@me.com>
2022-01-25 14:09:30 -0500

    prevent dark class from being purged

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,7 @@ module.exports = {
     './components/**/*.{js,ts,jsx,tsx,html}',
     './lib/**/*.{js,ts,jsx,tsx,html}',
   ],
+  safelist: ['dark'], //specific classes
   theme: {
     extend: {},
   },
