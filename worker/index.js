cb330057ffcc1202e9b41b05b165b0ce3f02324b
jasonwynn10 <jasonwynn10@me.com>
2022-01-27 13:24:28 -0500

    re-added firebase-messaging-sw.js

--- worker/index.js
+++ worker/index.js
@@ -1,52 +1,5 @@
 'use strict'
-
-// Give the service worker access to Firebase Messaging.
-// Note that you can only use Firebase Messaging here. Other Firebase libraries
-// are not available in the service worker.
-importScripts('https://www.gstatic.com/firebasejs/9.2.0/firebase-app-compat.js')
-importScripts(
-  'https://www.gstatic.com/firebasejs/9.2.0/firebase-messaging-compat.js'
-)
-
-// Initialize the Firebase app in the service worker by passing in
-// your app's Firebase config object.
-// https://firebase.google.com/docs/web/setup#config-object
-firebase.initializeApp({
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_PUBLIC_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-})
-
-// Retrieve an instance of Firebase Messaging so that it can handle background
-// messages.
-const messaging = firebase.messaging()
-
-// If you would like to customize notifications that are received in the
-// background (Web app is closed or not in browser focus) then you should
-// implement this optional method.
-// Keep in mind that FCM will still show notification messages automatically
-// and you should use data messages for custom notifications.
-// For more info see:
-// https://firebase.google.com/docs/cloud-messaging/concept-options
-messaging.onBackgroundMessage(function (payload) {
-  console.log(
-    '[firebase-messaging-sw.js] Received background message ',
-    payload
-  )
-  // Customize notification here
-  const notificationTitle = 'Background Message Title'
-  const notificationOptions = {
-    body: 'Background Message body.',
-    icon: '/manifest/icon-maskable.png',
-  }
-
-  self.registration.showNotification(notificationTitle, notificationOptions)
-})
+importScripts('firebase-messaging-sw.js')
 
 self.addEventListener('notificationclick', function (event) {
   event.notification.close()
