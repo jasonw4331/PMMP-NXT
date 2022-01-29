b95826a5ce10065b5842ead04acfef191f5b2d05
jasonwynn10 <jasonwynn10@me.com>
2022-01-28 20:12:49 -0500

    Added Blank settings pages

---
+++ pages/settings/profile.js
@@ -0,0 +1,17 @@
+import { AuthAction, withAuthUser } from 'next-firebase-auth'
+import Metatags from '../../components/Metatags'
+
+const YourProfile = () => {
+  return (
+    <>
+      <Metatags title='Settings' />
+      <ul className={'w-full flex flex-wrap justify-center lg:justify-start'}>
+        <li />
+      </ul>
+    </>
+  )
+}
+
+export default withAuthUser({
+  whenUnauthedAfterInit: AuthAction.REDIRECT_TO_LOGIN,
+})(YourProfile)
