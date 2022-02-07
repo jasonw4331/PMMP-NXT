9b4b6fd2d0df0c231d7a9ee9b39cf30135d1888a
jasonwynn10 <jasonwynn10@me.com>
2022-02-07 07:13:30 -0500

    Hide unusable auth providers

--- components/FirebaseAuth.js
+++ components/FirebaseAuth.js
@@ -27,7 +27,7 @@ const SignInButtons = () => {
     <div className={'w-full max-w-md flex flex-wrap justify-center gap-2'}>
       <button
         onClick={signInWithGoogle}
-        className='max-w-sm text-white bg-[#3469c1] hover:bg-[#3469c1]/90 focus:ring-4 focus:ring-[#4285F4]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 inline-flex items-center justify-center dark:focus:ring-[#4285F4]/55 dark:hover:bg-[#4285F4] mr-2 mb-2'>
+        className='max-w-sm text-white bg-[#3469c1] hover:bg-[#3469c1]/90 focus:ring-4 focus:ring-[#4285F4]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center justify-center dark:focus:ring-[#4285F4]/55 dark:hover:bg-[#4285F4] mr-2 mb-2'>
         <div className={'mr-2 -ml-1 w-4 h-4'}>
           <Image src={googleLogo} alt={'Google Logo'} />
         </div>
@@ -35,7 +35,8 @@ const SignInButtons = () => {
       </button>
       <button
         onClick={signInWithTwitter}
-        className='max-w-sm text-white bg-[#1da1f2] hover:bg-[#1da1f2]/90 focus:ring-4 focus:ring-[#1da1f2]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center justify-center dark:focus:ring-[#1da1f2]/55 mr-2 mb-2'>
+        disabled={true}
+        className='hidden max-w-sm text-white bg-[#1da1f2] hover:bg-[#1da1f2]/90 focus:ring-4 focus:ring-[#1da1f2]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center justify-center dark:focus:ring-[#1da1f2]/55 mr-2 mb-2'>
         <div className={'mr-2 -ml-1 w-4 h-4'}>
           <Image src={twitterLogo} alt={'Twitter Logo'} />
         </div>
@@ -43,7 +44,7 @@ const SignInButtons = () => {
       </button>
       <button
         onClick={signInWithGitHub}
-        className='max-w-sm text-white bg-[#24292F] hover:bg-[#24292F]/90 focus:ring-4 focus:ring-[#24292F]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 inline-flex items-center justify-center dark:focus:ring-gray-500 dark:hover:bg-[#414a55] mr-2 mb-2'>
+        className='max-w-sm text-white bg-[#24292F] hover:bg-[#24292F]/90 focus:ring-4 focus:ring-[#24292F]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center justify-center dark:focus:ring-gray-500 dark:hover:bg-[#414a55] mr-2 mb-2'>
         <div className={'mr-2 -ml-1 w-4 h-4'}>
           <Image src={githubMark} alt={'Github Mark'} />
         </div>
@@ -52,7 +53,7 @@ const SignInButtons = () => {
       <button
         onClick={signInWithGitLab}
         disabled={true}
-        className='hidden max-w-sm text-white bg-[#c6592a] hover:bg-[#ec6a32]/90 focus:ring-4 focus:ring-[#24292F]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 inline-flex items-center justify-center dark:focus:ring-gray-500 dark:hover:bg-[#ec6a32] mr-2 mb-2'>
+        className='hidden max-w-sm text-white bg-[#c6592a] hover:bg-[#ec6a32]/90 focus:ring-4 focus:ring-[#24292F]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center justify-center dark:focus:ring-gray-500 dark:hover:bg-[#ec6a32] mr-2 mb-2'>
         <div className={'mr-2 -ml-1 w-4 h-4'}>
           <Image src={gitlabIcon} alt={'GitLab Icon'} />
         </div>
@@ -61,7 +62,7 @@ const SignInButtons = () => {
       <button
         onClick={signInWithBitbucket}
         disabled={true}
-        className='hidden max-w-sm text-white bg-[#0747a6] hover:bg-[#0a67f2]/90 focus:ring-4 focus:ring-[#24292F]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 inline-flex items-center justify-center dark:focus:ring-gray-500 dark:hover:bg-[#0a67f2] mr-2 mb-2'>
+        className='hidden max-w-sm text-white bg-[#0747a6] hover:bg-[#0a67f2]/90 focus:ring-4 focus:ring-[#24292F]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center justify-center dark:focus:ring-gray-500 dark:hover:bg-[#0a67f2] mr-2 mb-2'>
         <div className={'mr-2 -ml-1 w-4 h-4'}>
           <Image src={bitbucketMark} alt={'Bitbucket Mark'} />
         </div>
--- pages/publish.js
+++ pages/publish.js
@@ -216,7 +216,7 @@ const SignInStep = ({ handleNext }) => {
         <button
           onClick={() => signInWithGitLab().then(handleNext)}
           disabled={true}
-          className='max-w-sm text-white bg-[#c6592a] hover:bg-[#ec6a32]/90 focus:ring-4 focus:ring-[#24292F]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 inline-flex items-center justify-center dark:focus:ring-zinc-500 dark:hover:bg-[#ec6a32] mr-2 mb-2'>
+          className='hidden max-w-sm text-white bg-[#c6592a] hover:bg-[#ec6a32]/90 focus:ring-4 focus:ring-[#24292F]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 inline-flex items-center justify-center dark:focus:ring-zinc-500 dark:hover:bg-[#ec6a32] mr-2 mb-2'>
           <div className={'mr-2 -ml-1 w-4 h-4'}>
             <Image src={gitlabIcon} alt={'GitLab Icon'} />
           </div>
@@ -225,7 +225,7 @@ const SignInStep = ({ handleNext }) => {
         <button
           onClick={() => signInWithBitbucket().then(handleNext)}
           disabled={true}
-          className='max-w-sm text-white bg-[#0747a6] hover:bg-[#0a67f2]/90 focus:ring-4 focus:ring-[#24292F]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 inline-flex items-center justify-center dark:focus:ring-zinc-500 dark:hover:bg-[#0a67f2] mr-2 mb-2'>
+          className='hidden max-w-sm text-white bg-[#0747a6] hover:bg-[#0a67f2]/90 focus:ring-4 focus:ring-[#24292F]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 inline-flex items-center justify-center dark:focus:ring-zinc-500 dark:hover:bg-[#0a67f2] mr-2 mb-2'>
           <div className={'mr-2 -ml-1 w-4 h-4'}>
             <Image src={bitbucketMark} alt={'Bitbucket Mark'} />
           </div>
