86381224319d8d80ba7be3757ed6d4a40ac6d93c
jasonwynn10 <jasonwynn10@me.com>
2022-02-08 02:48:04 -0500

    Fully separate site auth from git host data

--- pages/api/setCustomClaims.js
+++ pages/api/setCustomClaims.js
@@ -4,51 +4,91 @@ import { getFirebaseAdmin } from 'next-firebase-auth'
 initAuth()
 
 const handler = async (req, res) => {
+  async function testDeveloperClaims(claims) {
+    const data = await getFirebaseAdmin()
+      .firestore()
+      .collection('tokens')
+      .where('uid', '==', claims.sub)
+      .limit(1)
+      .get()
+    return data.docs.length > 0
+  }
+
+  function testReviewerClaims(claims) {
+    return JSON.parse(process.env.REVIEWERS).includes(claims.email)
+  }
+
+  function testAdminClaims(claims) {
+    return JSON.parse(process.env.ADMINS).includes(claims.email)
+  }
+
   try {
     const auth = getFirebaseAdmin().auth()
 
     // Get the ID token passed.
     const idToken = req.body.idToken
+    const checkClaims = req.body.checkClaims
 
     // Verify the ID token and decode its payload.
     const claims = await auth.verifyIdToken(idToken)
 
     let newClaims = {}
+    let newDocData = {}
 
     // Verify user is eligible for additional privileges.
-    if (claims.firebase.sign_in_provider === 'github.com') {
+    if (
+      checkClaims.includes('developer') &&
+      (await testDeveloperClaims(claims))
+    ) {
       // set developer privileges
       newClaims = {
         ...newClaims,
         developer: true,
       }
+      newDocData = {
+        ...newDocData,
+        type: 'developer',
+        followers: [],
+        recentReleases: [],
+        recentSubmissions: [],
+      }
     }
-    if (JSON.parse(process.env.REVIEWERS).includes(claims.email)) {
+    if (checkClaims.includes('reviewer') && testReviewerClaims(claims)) {
       // set reviewer privileges
       newClaims = {
         ...newClaims,
         reviewer: true,
       }
+      newDocData = {
+        ...newDocData,
+        type: 'reviewer',
+      }
     }
-    if (JSON.parse(process.env.ADMINS).includes(claims.email)) {
+    if (checkClaims.includes('admin') && testAdminClaims(claims)) {
       // set admin privileges
       newClaims = {
         ...newClaims,
         admin: true,
       }
+      newDocData = {
+        ...newDocData,
+        type: 'admin',
+      }
     }
-    if (newClaims !== {}) {
+    if (newClaims !== {})
       // Set custom claims on the user.
       await auth.setCustomUserClaims(claims.sub, newClaims)
-      return res.status(200).json({
-        status: 'success',
-      })
-    } else {
-      return res.status(200).json({
-        status: 'ineligible',
-        message: 'User is not eligible for additional privileges.',
-      })
-    }
+
+    if (newDocData !== {})
+      await getFirebaseAdmin()
+        .firestore()
+        .doc(`user/${claims.sub}`)
+        .update(newDocData)
+
+    return res.status(200).json({
+      status: 'success',
+      message: 'User privileges updated.',
+    })
   } catch (e) {
     console.error(e)
     return res.status(500).json({ error: 'Unexpected error.' })
