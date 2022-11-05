fd3850b6a3e552a82473900c2df37c1436ee719a
jasonwynn10 <jasonwynn10@me.com>
2022-11-05 03:49:24 -0400

    Added sidebar header and lowered width

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -2,7 +2,25 @@ import Link from 'next/link'
 
 export default function Sidebar() {
   return (
-    <ul className='menu p-4 overflow-y-auto w-80 bg-base-100 relative overflow-x-hidden'>
+    <ul className='menu p-4 overflow-y-auto w-60 bg-base-100 relative overflow-x-hidden'>
+      <div className='flex pl-0'>
+        <label htmlFor='my-drawer-3' className='btn btn-circle btn-ghost'>
+          <svg
+            xmlns='http://www.w3.org/2000/svg'
+            fill='none'
+            viewBox='0 0 24 24'
+            className='inline-block w-6 h-6 stroke-current'>
+            <path
+              strokeLinecap='round'
+              strokeLinejoin='round'
+              strokeWidth='2'
+              d='M4 6h16M4 12h16M4 18h16'></path>
+          </svg>
+        </label>
+        <Link href={'/'} className={'font-extrabold text-4xl ml-2'}>
+          NXT
+        </Link>
+      </div>
       <li>
         <a>Home</a>
       </li>
