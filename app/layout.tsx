68ace39ec08d25980928767d83fcc4f5a9c1f4e0
jasonwynn10 <jasonwynn10@me.com>
2022-11-05 03:52:40 -0400

    Added default page layout

---
+++ app/layout.tsx
@@ -0,0 +1,28 @@
+/* eslint-disable @next/next/no-head-element */
+import './globals.css'
+import Sidebar from '../components/Sidebar'
+import Navbar from '../components/Navbar'
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html>
+      <body>
+        <div className='drawer'>
+          <input id='my-drawer-3' type='checkbox' className='drawer-toggle' />
+          <div className='drawer-content flex flex-col'>
+            <Navbar />
+            <main>{children}</main>
+          </div>
+          <div className='drawer-side'>
+            <label htmlFor='my-drawer-3' className='drawer-overlay'></label>
+            <Sidebar />
+          </div>
+        </div>
+      </body>
+    </html>
+  )
+}
