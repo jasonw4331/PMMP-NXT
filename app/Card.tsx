50678758a443470ddd710df8f4e46d866c4b69ad
jasonwynn10 <jasonwynn10@me.com>
2022-11-04 22:02:52 -0400

    Added Plugin card element

---
+++ app/Card.tsx
@@ -0,0 +1,71 @@
+import Image, { StaticImageData } from 'next/image'
+import missingImage from '../public/icons/missing.png'
+import Link from 'next/link'
+
+export type CardData = {
+  id: string
+  name: string
+  author: string
+  tagline: string
+  iconUrl: string
+  downloadUrl: string
+  createdAt: number
+  lastUpdated: number
+  description: string
+  releaseStatus: number
+  minAPI: string
+  license: string
+  mainCategory: string
+  categories: string[]
+  subCategories: string[]
+  likes: string[]
+  dislikes: string[]
+}
+
+export default function Card({
+  name,
+  author,
+  tagline,
+  downloadUrl = '/404',
+  iconUrl = missingImage,
+}: {
+  name: string
+  author: string
+  tagline: string
+  downloadUrl: string
+  iconUrl: string | StaticImageData
+}) {
+  return (
+    <li className={'card w-96 bg-base-100 shadow-xl'}>
+      <figure className={'px-10 pt-10'}>
+        <Image
+          src={iconUrl}
+          alt='Plugin Icon'
+          width={205}
+          height={205}
+          placeholder={typeof iconUrl === 'string' ? 'empty' : 'blur'}
+          loading={'lazy'}
+          className={'rounded-xl'}></Image>
+      </figure>
+      <div className='card-body items-center text-center'>
+        <h2 className='card-title'>
+          {name.split('_v')[0]} by {author}
+        </h2>
+        <p>{tagline}</p>
+        <div className='card-actions'>
+          <button className='btn btn-primary'>
+            <Link
+              href={'/plugin/[username]/[plugin]'}
+              as={`/plugin/${encodeURI(author)}/${encodeURI(name)}`}
+              prefetch={false}>
+              More Details
+            </Link>
+          </button>
+          <button className='btn btn-primary'>
+            <Link href={downloadUrl}>Download</Link>
+          </button>
+        </div>
+      </div>
+    </li>
+  )
+}
