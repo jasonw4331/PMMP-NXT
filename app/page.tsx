2aaf3efedcba529c9b4039cded72679972aab780
jasonwynn10 <jasonwynn10@me.com>
2022-11-05 04:39:09 -0400

    Added Front Page and Header

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+import Card from '../components/Card'
+import getPlugins from '../lib/GetPlugin'
+
+export default async function HomePage() {
+  const data = await getPlugins()
+  return (
+    <ul className={'pt-2 flex flex-wrap gap-x-5 gap-y-10 justify-center'}>
+      {data.map(doc => (
+        <Card key={doc.id} name={doc.id} {...doc} />
+      ))}
+    </ul>
+  )
+}
