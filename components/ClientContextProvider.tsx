1510936e2785eeaccf4f3d45a6bfb3a317071676
jasonwynn10 <jasonwynn10@me.com>
2022-12-05 08:56:29 -0500

    Node children are always react nodes

--- components/ClientContextProvider.tsx
+++ components/ClientContextProvider.tsx
@@ -2,7 +2,11 @@
 import { UserContext, UserContextType } from '../lib/UserContext'
 import { useUserData } from '../lib/UserHooks'
 
-export default function ClientContextProvider({ children }: any) {
+export default function ClientContextProvider({
+  children,
+}: {
+  children: React.ReactNode
+}) {
   const userData: UserContextType = useUserData()
   return (
     <UserContext.Provider value={userData}>{children}</UserContext.Provider>
