e48fde88745df0a8cb64ee628110bc3d9033d5b4
jasonwynn10 <jasonwynn10@me.com>
2022-12-05 06:17:47 -0500

    vertically center search icon

--- components/navbar/SearchForm.tsx
+++ components/navbar/SearchForm.tsx
@@ -1,5 +1,6 @@
 'use client'
 import { useRouter } from 'next/navigation'
+import { MdSearch } from 'react-icons/all'
 
 export default function SearchForm() {
   const router = useRouter()
@@ -22,20 +23,8 @@ export default function SearchForm() {
           }}
           className={'input input-bordered pl-10 grow'}
         />
-        <div className={'absolute left-0 pl-2.5 pt-2.5 pointer-events-none'}>
-          <svg
-            xmlns='http://www.w3.org/2000/svg'
-            className={'h-6 w-6'}
-            fill='none'
-            viewBox='0 0 24 24'
-            stroke='currentColor'>
-            <path
-              strokeLinecap='round'
-              strokeLinejoin='round'
-              strokeWidth='2'
-              d='M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z'
-            />
-          </svg>
+        <div className={'absolute left-0 pl-2.5 pt-3 pointer-events-none'}>
+          <MdSearch className={'h-6 w-6'} width={24} height={24} />
         </div>
       </div>
     </div>
