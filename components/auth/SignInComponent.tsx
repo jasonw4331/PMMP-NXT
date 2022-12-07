58cff13ad4c09191aa3eae01ced478e75eaa9816
jasonwynn10 <jasonwynn10@me.com>
2022-12-07 06:04:41 -0500

    Added basic client login page component

---
+++ components/auth/SignInComponent.tsx
@@ -0,0 +1,91 @@
+'use client'
+import Link from 'next/link'
+import { FaGithub, FaGoogle, FaTwitter } from 'react-icons/fa'
+
+export default function SignInComponent() {
+  return (
+    <section className={'flex justify-center'}>
+      <form
+        className={
+          'form-control w-full max-w-xs lg:max-w-lg text-center gap-y-3'
+        }>
+        <Link href={'/'} className={'font-extrabold text-9xl uppercase shrink'}>
+          NXT
+        </Link>
+        <p className={'line-clamp-2'}>
+          The next generation of PocketMine-MP plugin distribution
+        </p>
+        <div>
+          <input
+            id='email'
+            type='email'
+            placeholder='Email Address'
+            className='input input-bordered input-invalid w-full'
+            pattern='^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$'
+            title='Please enter a valid email address'
+          />
+          <label className='label hidden input-invalid' htmlFor='email'>
+            <span className='label-text-alt text-left'>
+              Please enter a valid email address
+            </span>
+          </label>
+        </div>
+        <div>
+          <input
+            id='password'
+            type='password'
+            placeholder='Password'
+            className='input input-bordered input-invalid w-full'
+            pattern='(?=^.{6,}$)(?=.*\d)(?=.*[\p{Symbol}\p{Punctuation}]+)(?![.\n\r\t\s\b\p{Other}])(?=.*[A-Z])(?=.*[a-z]).*$'
+            title='Password must be at least 6 characters long, contain at least one uppercase letter, one lowercase letter, one number, and one special character.'
+          />
+          <label className='label hidden input-invalid' htmlFor='password'>
+            <span className='label-text-alt text-left'>
+              Password must be: &ge; 6 characters long, &ge; 1 uppercase letter,
+              1 lowercase letter, 1 number, and 1 special character.
+            </span>
+          </label>
+        </div>
+
+        <div className={'flex justify-between items-center'}>
+          <label className={'label cursor-pointer'}>
+            <span className='label-text'>Remember me</span>
+            <input type='checkbox' className='checkbox checkbox-info ml-3' />
+          </label>
+          <Link href='#!' className='link link-accent'>
+            Forgot password?
+          </Link>
+        </div>
+
+        <div className={'flex justify-around'}>
+          <button type='submit' className='btn btn-primary w-1/3'>
+            Sign Up
+          </button>
+          <button type='submit' className='btn btn-secondary w-1/3'>
+            Log in
+          </button>
+        </div>
+
+        <div className={'divider uppercase'}>or</div>
+
+        <div className='btn-group btn-group-vertical lg:btn-group-horizontal w-full justify-around'>
+          <button
+            className={
+              'btn grow bg-blue-500 hover:bg-blue-500/90 active:bg-blue-500/90'
+            }>
+            <FaTwitter size={32} />
+          </button>
+          <button
+            className={
+              'btn grow bg-orange-700 hover:bg-orange-700/90 active:bg-orange-700/90'
+            }>
+            <FaGoogle size={32} />
+          </button>
+          <button className={'btn grow'}>
+            <FaGithub size={32} />
+          </button>
+        </div>
+      </form>
+    </section>
+  )
+}
