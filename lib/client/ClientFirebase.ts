bb8145f24e22f0bc6f8bb4d62d1aaadb94295b2f
jasonwynn10 <jasonwynn10@me.com>
2023-01-05 06:48:57 -0500

    Corrected const type

--- lib/client/ClientFirebase.ts
+++ lib/client/ClientFirebase.ts
@@ -5,14 +5,7 @@ import {
   getApp,
   initializeApp,
 } from 'firebase/app'
-import {
-  Auth,
-  getAuth,
-  GithubAuthProvider,
-  GoogleAuthProvider,
-  signInWithRedirect,
-  TwitterAuthProvider,
-} from 'firebase/auth'
+import { Auth, getAuth } from 'firebase/auth'
 
 const projectId = process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID
 const databaseURL = `https://${projectId}.firebaseio.com`
@@ -21,7 +14,7 @@ const storageBucket = `${projectId}.appspot.com`
 const messagingSenderId =
   process.env.NEXT_PUBLIC_FIREBASE_APP_ID?.split(':')[1] || ''
 
-const firebaseConfig = {
+const firebaseConfig: FirebaseOptions = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_PUBLIC_API_KEY,
   authDomain,
   databaseURL,
@@ -30,7 +23,7 @@ const firebaseConfig = {
   messagingSenderId,
   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
   measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
-} as FirebaseOptions
+}
 
 function createFirebaseApp(config: FirebaseOptions): FirebaseApp {
   try {
@@ -44,18 +37,6 @@ const firebaseApp: FirebaseApp = createFirebaseApp(firebaseConfig)
 
 // Auth exports
 export const auth: Auth = getAuth(firebaseApp)
-
-export const signInWithTwitter = async () =>
-  await signInWithRedirect(auth, new TwitterAuthProvider())
-
-export const signInWithGoogle = async () =>
-  await signInWithRedirect(auth, new GoogleAuthProvider())
-
-export const signInWithGithub = async () =>
-  await signInWithRedirect(
-    auth,
-    new GithubAuthProvider().addScope('user:email')
-  )
 
 // Messaging exports
 //export const messaging: Messaging = getMessaging(firebaseApp)
