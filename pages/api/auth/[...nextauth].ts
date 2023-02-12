e178bb9249f613b2165080ad0f95cf4b87d5bf43
jasonwynn10 <jasonwynn10@me.com>
2023-02-12 15:41:57 -0500

    Update to fixed FirestoreAdapter

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,17 +1,16 @@
 import NextAuth, { AuthOptions } from 'next-auth'
 import GoogleProvider from 'next-auth/providers/google'
 import GithubProvider from 'next-auth/providers/github'
-import TwitterProvider from 'next-auth/providers/twitter'
 import { FirestoreAdapter } from '@next-auth/firebase-adapter'
-import { firebaseConfig } from '../../../lib/server/ServerFirebase'
+import { firestore } from '../../../lib/server/ServerFirebase'
 
-export const authOptions: AuthOptions = {
+// For more information on each option (and a full list of options) go to
+// https://next-auth.js.org/configuration/options
+export default NextAuth({
   callbacks: {
     async jwt({ token, account }) {
       // Persist the OAuth access_token to the token right after signin
-      if (account) {
-        token.accessToken = account.access_token
-      }
+      if (account !== null) token.userRole = 'user'
       return token
     },
     async session({ session, token, user }) {
@@ -20,55 +19,21 @@ export const authOptions: AuthOptions = {
       return session
     },
   },
-  adapter: FirestoreAdapter(firebaseConfig),
-  // Configure one or more authentication providers
+  adapter: FirestoreAdapter({ firestore }),
+  // https://next-auth.js.org/configuration/providers/oauth
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID as string,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
-      profile(profile) {
-        console.log(profile)
-        return {
-          id: profile.id,
-          name: profile.name,
-          email: profile.email,
-          image: profile.picture,
-        }
-      },
-      allowDangerousEmailAccountLinking: true,
     }),
     GithubProvider({
       clientId: process.env.GITHUB_APP_CLIENT_ID as string,
       clientSecret: process.env.GITHUB_APP_CLIENT_SECRET as string,
-      profile(profile) {
-        console.log(profile)
-        return {
-          id: profile.id.toString(),
-          name: profile.name ?? profile.login,
-          email: profile.email,
-          image: profile.avatar_url,
-        }
-      },
-      allowDangerousEmailAccountLinking: true,
-    }),
-    TwitterProvider({
-      clientId: process.env.TWITTER_CLIENT_ID as string,
-      clientSecret: process.env.TWITTER_CLIENT_SECRET as string,
-      profile(profile) {
-        console.log(profile)
-        return {
-          id: profile.id_str,
-          name: profile.name,
-          email: profile.email,
-          image: profile.profile_image_url_https.replace(
-            /_normal\.(jpg|png|gif)$/,
-            '.$1'
-          ),
-        }
-      },
     }),
     // ...add more providers here
   ],
-}
-
-export default NextAuth(authOptions)
+  secret: process.env.NEXTAUTH_SECRET,
+  theme: {
+    logo: process.env.NEXTAUTH_URL + '/icons/logo/icon.svg',
+  },
+} satisfies AuthOptions)
