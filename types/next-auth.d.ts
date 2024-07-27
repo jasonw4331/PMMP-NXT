631b22aef6d3e40b1b6f94441cc6c751517ef7c7
jasonw4331 <jasonwynn10@me.com>
2024-07-27 15:32:03 -0400

    user is nullable

--- types/next-auth.d.ts
+++ types/next-auth.d.ts
@@ -5,7 +5,7 @@ declare module 'next-auth' {
   interface Session extends DefaultSession {
     // A JWT which can be used as Authorization header with supabase-js for RLS.
     supabaseAccessToken?: string
-    user: {
+    user?: {
       /** The user's role. */
       role?: 'admin' | 'reviewer' | 'developer' | 'user'
     } & DefaultSession['user']
