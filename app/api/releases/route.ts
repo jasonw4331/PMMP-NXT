ef0a70de4fbff814f36170abbd24b8a3ed6cd591
jasonw4331 <jasonwynn10@me.com>
2024-07-27 15:32:41 -0400

    Add filler releases API

---
+++ app/api/releases/route.ts
@@ -0,0 +1,30 @@
+import { auth } from '@/auth'
+import { createClient } from '@supabase/supabase-js'
+import { Session } from 'next-auth'
+import Database from '@/database.types'
+
+export async function GET() {
+  const session: Session | null = await auth()
+  const { supabaseAccessToken } = session
+
+  const supabase = createClient<Database>(
+    process.env.NEXT_PUBLIC_SUPABASE_URL,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+    {
+      global: {
+        headers: {
+          Authorization: `Bearer ${supabaseAccessToken}`,
+        },
+      },
+    }
+  )
+  // Now we can query with RLS enabled.
+  const { data, error } = await supabase.from('releases').select('*')
+
+  // This is the default route for the search API and should return a json object with the search results
+  return new Response(JSON.stringify({}), {
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+}
