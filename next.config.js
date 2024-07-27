7410f465e8919648358e541c9caf3967b8a9095f
jasonw4331 <jasonwynn10@me.com>
2024-07-27 01:39:17 -0400

    type hints

--- next.config.js
+++ next.config.js
@@ -9,6 +9,7 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 })
 
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   images: {
