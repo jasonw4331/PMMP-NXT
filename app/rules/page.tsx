0dce3ca71ef90e4856408ada7d0b59d5fb2c6008
jasonw4331 <jasonwynn10@me.com>
2024-07-29 10:39:52 -0400

    Pre-render MDX on server side

--- app/rules/page.tsx
+++ app/rules/page.tsx
@@ -1,18 +1,30 @@
 import path from 'path'
 import fs from 'fs'
-import ReactMarkdown from 'react-markdown'
-import remarkGfm from 'remark-gfm'
+import { evaluate } from '@mdx-js/mdx'
+import * as runtime from 'react/jsx-runtime'
 
-export default function RulesPage() {
-  const postFilePath = path.join(process.cwd(), `public`, `rules.md`) // TODO: change to fetch from github md file
-  const source: string = fs.readFileSync(postFilePath, { encoding: 'utf8' })
+export default async function RulesPage() {
+  const postFilePath = path.join(process.cwd(), 'public', 'rules.md') // TODO: change to fetch from github md file
+  const mdxSource: string = fs.readFileSync(postFilePath, { encoding: 'utf8' })
 
+  // Run the compiled code
+  const {
+    default: MDXContent,
+    MDXDefinedComponent,
+    ...rest
+  } = await evaluate(mdxSource, runtime)
+
+  console.log(rest)
+
+  // Render the MDX content, supplying the ClientComponent as a component, and
+  // the exported MDXDefinedComponent.
   return (
     <article
       className={
         'mx-6 pt-6 lg:mx-24 lg:pt-24 prose prose-p:font-semibold prose-sm sm:prose-base md:prose-lg lg:prose-xl max-w-none'
       }>
-      <ReactMarkdown remarkPlugins={[remarkGfm]}>{source}</ReactMarkdown>
+      <MDXContent />
+      <MDXDefinedComponent />
     </article>
   )
 }
