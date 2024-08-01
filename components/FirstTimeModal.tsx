90e3adbf5a80c29a494447fae154ece869e474e7
jasonw4331 <jasonwynn10@me.com>
2024-07-31 20:39:11 -0400

    Fixed errors using server component on client

--- components/FirstTimeModal.tsx
+++ components/FirstTimeModal.tsx
@@ -1,6 +1,5 @@
-'use client'
 import { useEffect, useState } from 'react'
-import { signIn } from '@/auth'
+import { signIn } from 'next-auth/react'
 
 export default function FirstTimeModal() {
   const [isFirstTimeUser, setIsFirstTimeUser] = useState(
