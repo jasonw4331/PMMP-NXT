60774022a7dccbdedf639124139ee44a24a66f16
jasonw4331 <jasonwynn10@me.com>
2025-02-17 02:55:12 -0500

    Add avatars and releases to storage buckets

--- supabase/migrations/20250217040300_storage.sql
+++ supabase/migrations/20250217040300_storage.sql
@@ -1,11 +1,11 @@
 -- Use Postgres to create a bucket.
 
 insert into storage.buckets
-(id, name)
+(id, name, public)
 values
-    ('avatars', 'avatars');
+    ('avatars', 'avatars', true);
 
 insert into storage.buckets
-(id, name)
+(id, name, public)
 values
-    ('releases', 'releases');
+    ('releases', 'releases', true);
