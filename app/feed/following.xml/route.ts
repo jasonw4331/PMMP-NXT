0b90948e22370b8b87d057da6275c22bc30e3377
jasonw4331 <jasonwynn10@me.com>
2025-02-16 20:12:16 -0500

    Fixed old authed routes

--- app/feed/following.xml/route.ts
+++ app/feed/following.xml/route.ts
@@ -1,30 +1,31 @@
-import RSS from 'rss'
-import { auth } from '@/auth'
-import { NextResponse } from 'next/server'
+import RSS from "rss"
+import { type NextRequest, NextResponse } from "next/server"
+import { getUser } from "@/lib/auth/actions"
 
-export const GET = auth(async function GET(req, { params }) {
+export const GET = async (request: NextRequest) => {
+  const { user } = await getUser()
   const feed = new RSS({
-    title: 'Followed Plugins',
-    description: '',
-    generator: '',
+    title: "Followed Plugins",
+    description: "",
+    generator: "",
     feed_url: `https://${process.env.NEXT_BASE_URL}/feed/following.xml`,
     site_url: `https://${process.env.NEXT_BASE_URL}/`,
-    managingEditor: '',
-    webMaster: '',
+    managingEditor: "",
+    webMaster: "",
     copyright: `Copyright ${new Date().getFullYear().toString()}, PMMP NXT`,
-    language: 'en-US',
+    language: "en-US",
     pubDate: new Date().toUTCString(),
     ttl: 60,
   })
 
   const allPosts = [
     {
-      id: 'Plugin_v1.0.0',
-      title: 'Test',
-      description: 'Test',
+      id: "Plugin_v1.0.0",
+      title: "Test",
+      description: "Test",
       url: `https://${process.env.NEXT_BASE_URL}/posts/1`,
-      categories: ['test'],
-      tags: ['test'],
+      categories: ["test"],
+      tags: ["test"],
       date: new Date().toUTCString(),
     },
   ] //await getPostsMeta()
@@ -36,7 +37,7 @@ export const GET = auth(async function GET(req, { params }) {
         description: post.description,
         url: `https://${process.env.NEXT_BASE_URL}/posts/${post.id}`,
         categories: post.tags || [],
-        author: 'PMMP NXT',
+        author: "PMMP NXT",
         date: post.date,
       })
     })
@@ -44,7 +45,7 @@ export const GET = auth(async function GET(req, { params }) {
 
   return new NextResponse(feed.xml({ indent: true }), {
     headers: {
-      'Content-Type': 'application/xml; charset=utf-8',
+      "Content-Type": "application/xml; charset=utf-8",
     },
   })
-})
+}
