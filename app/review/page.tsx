6d6023f9387ad33490c50d95a7a87e9f93d6dfdd
jasonw4331 <jasonwynn10@me.com>
2025-02-16 19:57:44 -0500

    Fill in review page temp data

--- app/review/page.tsx
+++ app/review/page.tsx
@@ -1,26 +1,30 @@
-import ReleaseCard from '@/components/cards/ReleaseCard'
-import { Session } from 'next-auth'
-import { auth } from '@/auth'
-import { createClient } from '@supabase/supabase-js'
-import { type Database } from '@/types/Supabase'
-
-async function getData(count = 100) {
-  const session: Session | null = await auth()
-  const { supabaseAccessToken } = session
+import ReleaseCard from "@/components/cards/ReleaseCard"
+import { useCorbado } from "@corbado/react"
+import { redirect } from "next/navigation"
 
 export default async function ReviewPage() {
-  const submitted = [
-    {
-      id: 'TestPlugin_v1.0.0',
-      author: 'jasonwynn10',
-      tagline: 'This is a test plugin',
-    },
-  ] //await getSubmitted()
+  const releases = await getData()
   return (
-    <ul className={'pt-2 flex flex-wrap gap-x-5 gap-y-10 justify-center'}>
-      {submitted.map(doc => (
+    <ul className={"pt-2 flex flex-wrap gap-x-5 gap-y-10 justify-center"}>
+      {releases.map(doc => (
         <ReleaseCard key={doc.id} name={doc.id} {...doc} />
       ))}
     </ul>
   )
+}
+
+async function getData(count = 100) {
+  const { isAuthenticated } = useCorbado()
+
+  if (!isAuthenticated) {
+    redirect("/login")
+  }
+
+  return [
+    {
+      id: "TestPlugin_v1.0.0",
+      author: "jasonwynn10",
+      tagline: "This is a test plugin",
+    },
+  ]
 }
